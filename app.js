626197ef10a1440461759c7011ccc4de73bb7e5b
Rahul <rahulbokhare55@gmail.com>
2026-07-04 04:12:44 +0530

    Add create and edit listing functionality

--- app.js
+++ app.js
@@ -4,9 +4,12 @@ const mongoose = require("mongoose");
 const Listing = require("./models/listing")
 const ejs = require("ejs");
 const path = require("path");
+const methodOverride = require("method-override");
 
 app.set("view engine", "ejs")
 app.set("views",path.join(__dirname,"views"))
+app.use(express.urlencoded({ extended : true }))
+app.use(methodOverride("_method"))
 
 
 async function main(){
@@ -20,20 +23,48 @@ async function main(){
 }};
 main();
 
-//index route
-app.get("/home", async(req ,res) => {
+
+//Index route
+app.get("/home", async(req, res) => {
     let listings = await Listing.find();
-    res.render("index.ejs", { listings })
+    res.render("index.ejs", { listings });
+});
+//New Route
+app.get("/home/new", (req, res) => {
+    res.render("new.ejs")
 })
-
-
+app.post("/home/new", async(req, res) => {
+    let newListing = new Listing(req.body.listing);
+    await newListing.save();
+    console.log(newListing)
+    res.redirect;
+});
 //show route
-
 app.get("/home/:id", async(req, res) => {
     let { id } = req.params;
     let listing = await Listing.findById(id);
-    console.log(listing)
-    res.render("show.ejs", { listing })
+    res.render("show.ejs",{ listing })
 })
 
-
+//edit route
+app.get("/home/:id/edit", async(req, res) => {
+    let { id } = req.params;
+    listing = await Listing.findById(id);
+    res.render("edit.ejs", { listing });
+})
+app.put("/home/:id", async(req, res) => {
+    let { id } = req.params;
+    let listing = await Listing.findByIdAndUpdate(id, req.body.listing);
+    console.log(listing);
+    res.redirect(`/home/${id}`)
+})
+//delete route
+app.delete("/home/:id", async(req, res) => {
+    try{
+        let { id } = req.params;
+        await Listing.findByIdAndDelete(id);
+        res.redirect("/home");
+    }catch (e) {
+        console.log(e)
+    }
+})
